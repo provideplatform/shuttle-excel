70f68be52d40104ad074b9691f114c96938cbe8a
biscuitdey <biswashreedey@gmail.com>
2021-07-11 12:44:11 +0530

    New Commit

--- src/taskpane/excel-worker.ts
+++ src/taskpane/excel-worker.ts
@@ -20,6 +20,7 @@ export class ExcelWorker {
         }
 
         //this.renderWorkgroups(sheet, applications);
+        //Removed functionality
 
         if (active) {
           //sheet.activate();
